6eeb548daa07c1d32726e5fdf36e7c29655b38b0
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-27 20:46:57 +0330

    add server initialization with error handling

---
+++ server.js
@@ -0,0 +1,14 @@
+const { Users, Articles, Tags } = require("./models/Associations");
+const db = require("./db");
+const configs = require("./configs");
+const app = require("./app");
+
+(async () => {
+  try {
+    app.listen(configs.server.port, () => {
+      console.log(`Server Running On Port ${configs.server.port}`);
+    });
+  } catch (error) {
+    console.log("Error -->", error);
+  }
+})();
