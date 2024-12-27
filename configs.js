4f437709f335fdf43eb0e0f073d453714b7d537e
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-27 17:42:22 +0330

    add configuration for server and database settings

---
+++ configs.js
@@ -0,0 +1,19 @@
+module.exports = {
+  server: {
+    port: process.env.SERVER_PORT,
+  },
+
+  DB: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+
+  auth: {
+    accessTokenSecretKey: process.env.ACCESS_TOKEN_SECRET_KEY,
+    refreshTokenSecretKey: process.env.REFRESH_TOKEN_SECRET_KEY,
+    accessTokenExpireInSecond: process.env.ACCESS_TOKEN_EXPIRE_IN_SECOND,
+    refreshTokenExpireInSecond: process.env.REFRESH_TOKEN_EXPIRE_IN_SECOND,
+  },
+};
