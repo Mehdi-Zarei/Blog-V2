e6b9bc5642a2e1d3aca9feaa2d209fafd0f71edc
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-29 18:48:31 +0330

    update database connection in db.js to use a hardcoded URI for testing purposes

--- db.js
+++ db.js
@@ -1,20 +1,38 @@
 const configs = require("./configs");
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  host: configs.DB.host,
-  username: configs.DB.username,
-  password: configs.DB.password,
-  database: configs.DB.database,
-  dialect: "mysql",
-  logging: false, //console.log,
+//* Way 1
+
+// const sequelize = new Sequelize(
+//   `mysql://${configs.DB.username}:${configs.DB.password}@${configs.DB.host}/${configs.DB.database}`,
+//   {
+//     logging: false, // Optional: For disabling logging
+//   }
+// );
+
+//* Way 2
+
+// const sequelize = new Sequelize({
+//   host: configs.DB.host,
+//   username: configs.DB.username,
+//   password: configs.DB.password,
+//   database: configs.DB.database,
+//   dialect: "mysql",
+//   logging: false, //console.log,
+// });
+
+//* Way 3
+
+const sequelize = new Sequelize(`mysql://root:@localhost:3306/BLOG_V2`, {
+  logging: false, // Optional: Disable logging
 });
+
+///////////////////////////////////////////
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connect To DB Successfully.");
+    console.log(`Connect To ${configs.DB.database} DB Successfully.`);
   } catch (error) {
     await sequelize.close();
     console.error("Unable to connect to the database:", error);
