a2b3465f3d982ba8e590198a3310aea43d40d719
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-30 11:57:29 +0330

    add: establish associations between Users, Articles, and Tags models

--- models/Associations.js
+++ models/Associations.js
@@ -4,12 +4,39 @@ const Tags = require("./Tags");
 
 Users.hasMany(Articles, {
   foreignKey: "author_id",
+  as: "author",
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
 
 Articles.belongsTo(Users, {
   foreignKey: "author_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+Users.hasMany(Tags, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+Tags.belongsTo(Users, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+Articles.belongsToMany(Tags, {
+  through: "ArticlesTags",
+  foreignKey: "tags_id",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+Tags.belongsToMany(Articles, {
+  through: "ArticlesTags",
+  foreignKey: " articles_id",
   onDelete: "CASCADE",
   onUpdate: "CASCADE",
 });
