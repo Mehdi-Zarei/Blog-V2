330d10721f129db217f6388c929c177fa4341450
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-30 11:57:49 +0330

    add: implement registration route with validation middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
+
+const validateSchema = require("../validators/auth");
+const validate = require("../middlewares/authValidate");
+const authController = require("../controllers/auth");
+
+router
+  .route("/register")
+  .post(validate(validateSchema), authController.register);
 
 module.exports = router;
