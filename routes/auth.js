ab070b99cde913251024ed731934499191a4963f
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-30 17:06:14 +0330

    feat: add login route with local authentication and validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const validateSchema = require("../validators/auth");
 const validate = require("../middlewares/authValidate");
 const authController = require("../controllers/auth");
+const loginValidatorSchema = require("../validators/login");
 
 router
   .route("/register")
   .post(validate(validateSchema), authController.register);
+
+router
+  .route("/login")
+  .post(
+    validate(loginValidatorSchema),
+    passport.authenticate("local", { session: false }),
+    authController.login
+  );
 
 module.exports = router;
