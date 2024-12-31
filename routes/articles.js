c4fb4d7771872cf14558bb550b1721f6a368b843
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-31 16:15:34 +0330

    feat: add JWT authentication to article creation route

--- routes/articles.js
+++ routes/articles.js
@@ -4,7 +4,14 @@ const router = express.Router();
 const validator = require("../middlewares/bodyValidate");
 const articleSchema = require("../validators/createArticle");
 const articleController = require("../controllers/articles");
+const passport = require("passport");
 
-router.route("/").post(validator(articleSchema), articleController.create);
+router
+  .route("/")
+  .post(
+    passport.authenticate("accessToken", { session: false }),
+    validator(articleSchema),
+    articleController.create
+  );
 
 module.exports = router;
