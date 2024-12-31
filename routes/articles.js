4a0076adb002c6f42dbaf454a115f0f40fae6535
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-31 15:27:10 +0330

    feat: add article creation route with body validation

--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
+
+const validator = require("../middlewares/bodyValidate");
+const articleSchema = require("../validators/createArticle");
+const articleController = require("../controllers/articles");
+
+router.route("/").post(validator(articleSchema), articleController.create);
 
 module.exports = router;
