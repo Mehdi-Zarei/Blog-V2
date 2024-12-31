fe4cacf7bd3f394f80a125a5fcbc340ce355ee0d
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-31 20:20:08 +0330

    feat: enhance article creation with slug generation and cover image handling

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,12 +1,38 @@
+const fs = require("fs");
+const articlesModel = require("../models/Articles");
+const TagsModel = require("../models/Tags");
+const slugify = require("slugify");
+
 exports.create = async (req, res, next) => {
   try {
-    // const { title, description, content } = req.body;
+    let { title, description, content, tags } = req.body;
 
-    console.log("req body -->", req.body);
+    const author = await req.user;
+
+    tags = Array.isArray(tags) ? tags : [tags];
+
+    const slug = slugify(title, { lower: true });
+
+    // tags = tags.map((tag) =>
+    //   TagsModel.findOrCreate({ where: { title: tag.trim() } })
+    // );
+    // tags = await Promise.all(tags);
+
+    const coverPath = `public/images/covers/${req.file?.filename}`;
+
+    const newArticles = await articlesModel.create({
+      title,
+      description,
+      content,
+      slug,
+      publish: false,
+      author_id: author.id,
+      cover: req.file ? coverPath : null,
+    });
 
     return res
       .status(201)
-      .json({ message: "New Article Created Successfully." });
+      .json({ message: "New Article Created Successfully.", newArticles });
   } catch (error) {
     next(error);
   }
