4968cc14295b4dd9b6a71ab15b582d0c67099ccc
Mehdi zarei <mahdizareiofficial@gmail.com>
2024-12-31 20:20:26 +0330

    feat: update tags validation to accept string or array of strings

--- validators/createArticle.js
+++ validators/createArticle.js
@@ -13,21 +13,33 @@ const articleSchema = yup.object().shape({
 
   content: yup.string().required("Description is required"),
 
-  tags: yup
-    .array()
-    .of(
-      yup
-        .string()
-        .matches(
-          /^[\u0600-\u06FFa-zA-Z-_]+$/,
-          "Tag must only contain Persian/English letters, dashes, or underscores, and cannot contain numbers"
-        )
-    )
-    .required("Tags are required")
-    .min(1, "At least one tag is required")
-    .test("no-numbers", "Tags cannot contain numeric values", (tags) =>
-      tags.every((tag) => !/^\d+$/.test(tag))
-    ),
+  // tags: yup
+  //   .array()
+  //   .of(
+  //     yup
+  //       .string()
+  //       .matches(
+  //         /^[\u0600-\u06FFa-zA-Z-_]+$/,
+  //         "Tag must only contain Persian/English letters, dashes, or underscores, and cannot contain numbers"
+  //       )
+  //   )
+  //   .required("Tags are required")
+  //   .min(1, "At least one tag is required")
+  //   .test("no-numbers", "Tags cannot contain numeric values", (tags) =>
+  //     tags.every((tag) => !/^\d+$/.test(tag))
+  //   ),
+
+  tags: yup.mixed().test(
+    "is-string-or-array",
+    "Tags must be a string or an array of strings", // ["frontend", 12]
+    (value) => {
+      return (
+        typeof value === "string" ||
+        (Array.isArray(value) &&
+          value.every((item) => typeof item === "string"))
+      );
+    }
+  ),
 });
 
 module.exports = articleSchema;
