74b0d4788772bf928cf942a19d012986b2c7f43b
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-01-06 18:22:28 +0330

    Refactor password reset functionality to use UUID for token generation and improve Redis token management

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 const userModel = require("../models/Users");
-const { Op, UUID } = require("sequelize");
+const { Op } = require("sequelize");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const configs = require("../configs");
 const redis = require("../redis");
 const sendOtp = require("../utils/sendOtp");
+const uuid = require("uuid").v4;
 const { sendVerificationEmail } = require("../utils/nodemailer");
 
 exports.register = async (req, res, next) => {
@@ -179,16 +180,20 @@ exports.forgetPassword = async (req, res, next) => {
         .json({ message: "User Not Found With This Email !!" });
     }
 
-    const resetPasswordToken = configs.nodemailer.generateResetPasswordToken;
+    const resetPasswordToken = uuid();
 
-    await redis.set(`resetPassword:${email}`, resetPasswordToken, "EX", 3600);
+    await redis.hset(`resetPassword:${resetPasswordToken}`, {
+      token: resetPasswordToken,
+      email: email,
+    });
+    await redis.expire(`resetPassword:${resetPasswordToken}`, 3600); //* 1 Hour
 
     const subject = "Reset Password Link";
 
     const text = `
     <p>سلام ${user.name} عزیز،</p>
     <p>درخواست تغییر رمز عبور برای حساب کاربری شما ثبت شده است. اگر این درخواست از سمت شما بوده است، لطفاً از طریق لینک زیر اقدام به تغییر رمز عبور خود کنید:</p>
-    <a href="http://localhost:${configs.server.port}/auth/reset-password/${configs.nodemailer.generateResetPasswordToken}">تغییر رمز عبور</a>
+    <a href="http://localhost:${configs.server.port}/auth/reset-password/${resetPasswordToken}">تغییر رمز عبور</a>
     <p>این لینک تا <strong>۱ ساعت</strong> آینده معتبر است. اگر شما این درخواست را ارسال نکرده‌اید، لطفاً این ایمیل را نادیده بگیرید یا با پشتیبانی تماس بگیرید.</p>
     <p>با تشکر،</p>
     <p>تیم پشتیبانی</p>
@@ -212,7 +217,34 @@ exports.forgetPassword = async (req, res, next) => {
 
 exports.resetPassword = async (req, res, next) => {
   try {
-    const { email } = req.body;
+    const { password } = req.body;
+
+    const { token } = req.params;
+
+    const data = await redis.hgetall(`resetPassword:${token}`);
+
+    if (!data || data.token !== token) {
+      return res
+        .status(404)
+        .json({ message: "Invalid or expired reset token." });
+    }
+
+    const hashedNewPassword = bcrypt.hashSync(password, 12);
+
+    const [user] = await userModel.update(
+      { password: hashedNewPassword },
+      { where: { email: data.email } }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found !!" });
+    }
+
+    await redis.del(`resetPassword:${token}`);
+
+    return res
+      .status(200)
+      .json({ message: "Your Password Changed Successfully." });
   } catch (error) {
     next(error);
   }
